e330407f00f69cfd11e901361ff17e907a230f53
Raphael Gomez <raphael.gomezserfaty@gmail.com>
2024-04-24 14:33:17 +0200

    refactor and cross behavior

--- newtab.js
+++ newtab.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     getLocation();
-    displayTileCount()
-
+    displayTileCount();
+    buildListeners()
 });
 
 const tileTypes = ['grass', 'soil', 'rocks', 'river', 'wheat'];
@@ -25,6 +25,29 @@ const shopItems = [
 
 ];
 
+// Build click listeners
+
+function buildListeners(){
+    document.getElementById('resetButton').addEventListener('click', function() {
+        chrome.storage.local.get({lifetimeTreeCount: 0, coins: 0}, function(data) {
+            // Reset only the grid counter, not the lifetime counter or coins
+            chrome.storage.local.set({gridTreeCount: 0}, function() {
+                updateForestDisplay(0);
+                updateTreeCounterDisplay(0, data.lifetimeTreeCount);
+            });
+        });
+    });
+    document.getElementById('shopButton').addEventListener('click', function() {
+        document.getElementById('shopOverlay').style.display = 'flex';
+        populateShop();
+    });
+    document.getElementById('overlay-cross').addEventListener('click', function() {
+        document.getElementById('shopOverlay').style.display = 'none';
+    });
+    document.getElementById('motherlodeButton').addEventListener('click', motherlode)
+}
+
+// Weather-related functions
 
 function getLocation() {
     if (navigator.geolocation) {
@@ -138,11 +161,8 @@ function updateForestDisplay(count) {
         const forestType = getRandomItem(["forest-1", "forest-autumn", "forest-2"])
         //const svgFilePath = `assets/SVG/${forestType}.svg`
         const svgFilePath = `assets/SVG/forest-tile-naked.svg`
-
         // Call fetchAndDisplaySVG for each tile
         fetchAndDisplaySVG(svgFilePath, forestElement, 150, 100, row, col);
-
-        //fetchAndDisplayPNG(tilePath, forestElement, 128, 128, row, col);
     }
 }
 
@@ -175,36 +195,11 @@ function fetchAndDisplaySVG(svgFilePath, containerElement, width, height, row, c
 }
 
 
-function fetchAndDisplayPNG(path, containerElement, width, height, row, col) {
-    const img = document.createElement('img');
-    img.src = path;
-    img.style.width = `${width}px`; // Set width for the tile
-    img.style.height = `${height}px`; // Set height for the tile
-
-    // Calculate isometric positions
-    const isoX = (col - row) * width / 2;
-    const isoY = (col + row) * height / 4; // Dividing by 4 reduces the vertical overlap
-
-    img.style.position = 'absolute';
-    img.style.left = `${isoX + containerElement.offsetWidth / 2 - width / 2}px`; // Centering the grid
-    img.style.top = `${isoY}px`;
-
-    containerElement.appendChild(img);
-    containerElement.style.position = 'relative';
-    containerElement.style.height = `${isoY + height}px`; // Ensure the container is tall enough
-}
-
-
-
 function updateTreeCounterDisplay(gridCount, lifetimeCount, coins) {
     document.getElementById('treeCounter').textContent = `Trees: ${lifetimeCount}`; // Show lifetime count
 }
 
-function updateCoinDisplay(coins){
-    document.getElementById('coinCounter').textContent = `Coins: ${coins}`;
-}
-
-// MONEY HANDLING
+// SHOP AND MONEY HANDLING
 
 function getCoins() {
     const coins = localStorage.getItem('coins');
@@ -227,15 +222,16 @@ function addPurchasedItem(itemId) {
     updateCoinDisplay(coinCount);
 }
 
-document.getElementById('shopButton').addEventListener('click', function() {
-    document.getElementById('shopOverlay').style.display = 'flex';
-    populateShop();
-});
+function updateCoinDisplay(coins){
+    document.getElementById('coinCounter').textContent = `Coins: ${coins}`;
+}
 
 function populateShop() {
     const grid = document.getElementById('shopGrid');
     grid.innerHTML = ''; // Clear previous items
     const purchasedItems = getPurchasedItems();
+    const exitShopBtn = document.getElementById('overlay-cross');
+
 
     shopItems.forEach(item => {
         const itemDiv = document.createElement('div');
@@ -309,29 +305,8 @@ function incrementTreeCounter() {
             updateCoinDisplay(newCoins);
         });
     });
-}
 
-function loadSVG(tileType, state) {
-    const path = `assets/tiles/${tileType}/${state}.svg`;
-    fetch(path)
-        .then(response => response.text())
-        .then(svg => {
-            document.getElementById('your-target-element').innerHTML = svg;
-        });
-}
-
-// Reset button functionality
-document.getElementById('resetButton').addEventListener('click', function() {
-    chrome.storage.local.get({lifetimeTreeCount: 0, coins: 0}, function(data) {
-        // Reset only the grid counter, not the lifetime counter or coins
-        chrome.storage.local.set({gridTreeCount: 0}, function() {
-            updateForestDisplay(0);
-            updateTreeCounterDisplay(0, data.lifetimeTreeCount);
-        });
-    });
-});
-
-document.getElementById('motherlodeButton').addEventListener('click', motherlode)
+// UTILITIES
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.action === "newTab") {

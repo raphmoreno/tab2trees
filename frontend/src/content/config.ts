d2cdad166120a08290e106b5ec9f302b30c45827
Raphael Gomez <raphael.gomezserfaty@gmail.com>
2024-07-09 17:44:27 +0200

    updated debug mode

--- frontend/src/content/config.ts
+++ frontend/src/content/config.ts
@@ -5,7 +5,7 @@ export const config = {
     cellWidth: 30,
     canvasHeight: 500,
     canvasWidth: 750,
-    isDebugMode: true,
+    isDebugMode: false,
     tileTypes: ['forest-1', 'forest-2', 'forest-autumn'],
     treeConfig: ['pine', 'oak-spring', 'oak-autumn', 'cherry-blossom-summer', 'cherry-blossom-spring']
 };
